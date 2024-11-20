edac0c3e64cfc29cb247066cd30a90840c05d828
gla9931 <jarredglaser@northwesternmutual.com>
2024-11-20 15:56:39 -0600

    Use hashrouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,37 +1,34 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createHashRouter, RouterProvider } from "react-router-dom";
 import Home from "./components/Home.tsx";
 import TablePage from "./components/TablePage.tsx";
 import ErrorPage from "./ErrorPage.tsx";
 import "./index.css";
 import Root from "./Root.tsx";
 
-const router = createBrowserRouter(
-  [
-    {
-      path: "/",
-      element: <Root />,
-      errorElement: <ErrorPage />,
-      children: [
-        {
-          path: "/",
-          children: [
-            {
-              path: "tables/:category",
-              element: <TablePage />,
-            },
-            {
-              path: "",
-              element: <Home />,
-            },
-          ],
-        },
-      ],
-    },
-  ],
-  { basename: "/ttrpg-generators/" }
-);
+const router = createHashRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/",
+        children: [
+          {
+            path: "tables/:category",
+            element: <TablePage />,
+          },
+          {
+            path: "",
+            element: <Home />,
+          },
+        ],
+      },
+    ],
+  },
+]);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
